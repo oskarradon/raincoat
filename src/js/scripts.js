ff732c01ea5f31a49f4d08d4cf0101245884c9e5
Oskar <oskarradonkimball@gmail.com>
2016-08-31 23:41:52 -0700

    Refactor API call to use promise

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,10 @@
 var apiKey = "eef54fef4f484053c798c7b450ab75ab";
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-	geoLocate();
+	getLocation();
 });
 
-function geoLocate () {
+function getLocation () {
 	// get lat and long coordinates from browser
 	navigator.geolocation.getCurrentPosition(success, error);
 
@@ -20,22 +20,41 @@ function geoLocate () {
 	};
 }
 
-function callAPI (lat, long) {
-	var request = new XMLHttpRequest();
-	request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + apiKey, true);
+// function callAPI (lat, long) {
+// 	var request = new XMLHttpRequest();
+// 	request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + apiKey, true);
 
-	request.onload = function() {
-		if (request.status >= 200 && request.status < 400) {
-			var data = JSON.parse(request.responseText);
-			console.log(data);
-		} else {
-			console.log('server error ', request.status)
-		}
-	};
+// 	request.onload = function() {
+// 		if (request.status >= 200 && request.status < 400) {
+// 			var data = JSON.parse(request.responseText);
+// 			console.log(data);
+// 			document.getElementById('location').textContent = data.name;
+// 		} else {
+// 			console.log('server error ', request.status)
+// 		}
+// 	};
 
-	request.onerror = function() {
-		console.log('API connection error')
-	};
+// 	request.onerror = function() {
+// 		console.log('API connection error')
+// 	};
 
-	request.send();
+// 	request.send();
+// }
+
+function callAPI(lat, long) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      resolve(JSON.parse(this.responseText));
+    };
+    xhr.onerror = reject;
+    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + apiKey);
+    xhr.send();
+  });
 }
+
+callAPI().then(function(result) {
+  console.log(result);
+}).catch(function() {
+  // An error occurred
+});
