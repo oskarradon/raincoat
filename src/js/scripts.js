2330619d83defe7f68d2d666a1f9bd698c59d319
Oskar <oskarradonkimball@gmail.com>
2016-09-01 10:42:42 -0700

    Change to one result div

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,28 +3,25 @@ function getLocation () {
 	function success(position) {
 		var latitude  = position.coords.latitude;
 		var longitude = position.coords.longitude;
-		console.log(latitude, longitude);
 		getWeather(latitude, longitude); // get weather
 	};
 	function error() {
 		//Couldn't find location
-  	document.getElementById('loading').classList.add('hidden');
-		document.getElementById('location-error').classList.add('showing');
-		console.log('GPS error')
+  	document.getElementById('loading').style.display = 'none';
+		document.getElementById('location-error').style.display = 'block';
 	};
 }
 
 function getWeather (lat, long) {
 	var apiKey = "eef54fef4f484053c798c7b450ab75ab";
 	var request = new XMLHttpRequest();
-	request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + apiKey, true);
+	request.open('GET', 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + long + '&appid=' + apiKey, true);
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400) {
 			var data = JSON.parse(request.responseText);
 			console.log(data);
-			// document.getElementById('loading').classList.add('hidden');
-			document.getElementById('raining').classList.add('showing');
-			console.log(document.getElementById('raining').classList)
+			document.getElementById('loading').style.display = 'none';
+			document.getElementById('raining').style.display = 'block';
 		} else {
 			// Couldnt get the weather
 			console.log('server error ', request.status)
