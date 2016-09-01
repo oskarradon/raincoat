29e7a9aee6b2b1d6c9c3071d84f442f641f62d3c
Oskar <oskarradonkimball@gmail.com>
2016-09-01 11:02:13 -0700

    Finish js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,7 +8,8 @@ function getLocation () {
 	function error() {
 		//Couldn't find location
   	document.getElementById('loading').style.display = 'none';
-		document.getElementById('location-error').style.display = 'block';
+		document.getElementById('result').style.display = 'block';
+		document.getElementById('result-text').innerHTML = "Woah, couldn't find your location!"
 	};
 }
 
@@ -21,14 +22,22 @@ function getWeather (lat, long) {
 			var data = JSON.parse(request.responseText);
 			console.log(data);
 			document.getElementById('loading').style.display = 'none';
-			document.getElementById('raining').style.display = 'block';
+			document.getElementById('result').style.display = 'block';
+			console.log(data.list[0].weather[0].main )
+			if (data.list[0].weather[0].main === "Rain" || data.list[1].weather[0].main === "Rain" || data.list[2].weather[0].main === "Rain" ) {
+				document.getElementById('result-text').innerHTML = "Yep, " + data.city.name + " weather isn't looking too good."
+			}
 		} else {
-			// Couldnt get the weather
+			document.getElementById('loading').style.display = 'none';
+			document.getElementById('result').style.display = 'block';
+			document.getElementById('result-text').innerHTML = "Whoops, couldn't check the weather."
 			console.log('server error ', request.status)
 		}
 	};
 	request.onerror = function() {
-		// Couldnt get the weather
+		document.getElementById('loading').style.display = 'none';
+		document.getElementById('result').style.display = 'block';
+		document.getElementById('result-text').innerHTML = "Whoops, couldn't check the weather."
 		console.log('API connection error')
 	};
 	request.send();
