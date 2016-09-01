b6b31a8c7573be47c62754085fa0aa4dfc8c0636
Oskar <oskarradonkimball@gmail.com>
2016-09-01 08:47:57 -0700

    Promise chaining not working

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,60 +1,39 @@
 var apiKey = "eef54fef4f484053c798c7b450ab75ab";
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-	getLocation();
+document.addEventListener("DOMContentLoaded", function(event) {
+
+	getLocation().then((position) => {
+		console.log(position);
+		return position;
+	}).then((position) => {
+		var lat  = position.coords.latitude;
+		var long = position.coords.longitude;
+		getWeather(lat, long).then((response) => {
+			console.log(response);
+		});	
+	});
 });
 
 function getLocation () {
-	// get lat and long coordinates from browser
-	navigator.geolocation.getCurrentPosition(success, error);
-
-	function success(position) {
-		var latitude  = position.coords.latitude;
-		var longitude = position.coords.longitude;
-		console.log(latitude, longitude);
-		callAPI(latitude, longitude);
-	};
-
-	function error() {
-		console.log('GPS error')
-	};
+	return new Promise(function(resolve, reject) {
+		navigator.geolocation.getCurrentPosition(success, error);
+		function success(position) {
+			resolve(position);
+		}
+		function error() {
+			reject();
+		}
+	});
 }
 
-// function callAPI (lat, long) {
-// 	var request = new XMLHttpRequest();
-// 	request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + apiKey, true);
-
-// 	request.onload = function() {
-// 		if (request.status >= 200 && request.status < 400) {
-// 			var data = JSON.parse(request.responseText);
-// 			console.log(data);
-// 			document.getElementById('location').textContent = data.name;
-// 		} else {
-// 			console.log('server error ', request.status)
-// 		}
-// 	};
-
-// 	request.onerror = function() {
-// 		console.log('API connection error')
-// 	};
-
-// 	request.send();
-// }
-
-function callAPI(lat, long) {
-  return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-      resolve(JSON.parse(this.responseText));
-    };
-    xhr.onerror = reject;
-    xhr.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + apiKey);
-    xhr.send();
-  });
+function getWeather(lat, long) {
+	return new Promise(function(resolve, reject) {
+		var xhr = new XMLHttpRequest();
+		xhr.onload = function() {
+			resolve(JSON.parse(this.responseText));
+		};
+		xhr.onerror = reject;
+		xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=" + apiKey);
+		xhr.send();
+	});
 }
-
-callAPI().then(function(result) {
-  console.log(result);
-}).catch(function() {
-  // An error occurred
-});
