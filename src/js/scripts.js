c13cc238be5322f35b3712a668cf2da91710badb
Oskar <oskarradonkimball@gmail.com>
2016-09-01 09:58:03 -0700

    Revert to not using promises

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,38 +1,35 @@
-var apiKey = "eef54fef4f484053c798c7b450ab75ab";
-
-document.addEventListener("DOMContentLoaded", function(event) {
-	getLocation().then((position) => {
-		console.log(position);
-		return position;
-	}).then((position) => {
-		var lat  = position.coords.latitude;
-		var long = position.coords.longitude;
-		getWeather(lat, long).then((response) => {
-			console.log(response);
-		});	
-	});
-});
-
 function getLocation () {
-	return new Promise(function(resolve, reject) {
-		navigator.geolocation.getCurrentPosition(success, error);
-		function success(position) {
-			resolve(position);
-		}
-		function error() {
-			reject();
-		}
-	});
+	navigator.geolocation.getCurrentPosition(success, error);
+	function success(position) {
+		var latitude  = position.coords.latitude;
+		var longitude = position.coords.longitude;
+		console.log(latitude, longitude);
+		getWeather(latitude, longitude);
+	};
+	function error() {
+		//Couldn't find location
+		console.log('GPS error')
+	};
 }
 
-function getWeather(lat, long) {
-	return new Promise(function(resolve, reject) {
-		var xhr = new XMLHttpRequest();
-		xhr.onload = function() {
-			resolve(JSON.parse(this.responseText));
-		};
-		xhr.onerror = reject;
-		xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=" + apiKey);
-		xhr.send();
-	});
+function getWeather (lat, long) {
+	var apiKey = "eef54fef4f484053c798c7b450ab75ab";
+	var request = new XMLHttpRequest();
+	request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + apiKey, true);
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400) {
+			var data = JSON.parse(request.responseText);
+			console.log(data);
+		} else {
+			// Couldnt get the weather
+			console.log('server error ', request.status)
+		}
+	};
+	request.onerror = function() {
+		// Couldnt get the weather
+		console.log('API connection error')
+	};
+	request.send();
 }
+
+getLocation();
